7224de343344e787cc1ebccee99b027051f9ad52
Hollow-YK <normal.wzj@qq.com>
2026-02-21 12:42:00 +0800

    small changes

--- src/endfieldindustrylib/AICTransport/AICTransport.java
+++ src/endfieldindustrylib/AICTransport/AICTransport.java
@@ -2,10 +2,10 @@ package endfieldindustrylib.AICTransport;
 
 public class AICTransport {
     public static void load() {
-        new TransportBelt("endfield-transport-belt").load();
-        new ItemControlPort("endfield-item-control-port").load();
-        new Splitter("endfield-splitter").load();
-        new BeltBridge("endfield-belt-bridge").load();
-        new Converger("endfield-converger").load();
+        new TransportBelt("transport-belt").load();
+        new ItemControlPort("item-control-port").load();
+        new Splitter("splitter").load();
+        new BeltBridge("belt-bridge").load();
+        new Converger("converger").load();
     }
 }
