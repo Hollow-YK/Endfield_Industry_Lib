083148b9eccda32a294d39cb216b640f1b51f25d
Hollow-YK <normal.wzj@qq.com>
2026-02-21 14:23:37 +0800

    Add: items

--- src/endfieldindustrylib/Lib.java
+++ src/endfieldindustrylib/Lib.java
@@ -3,19 +3,28 @@ package endfieldindustrylib;
 import endfieldindustrylib.AICTransport.*;
 import endfieldindustrylib.Items.Ore;
 import endfieldindustrylib.Items.Plants;
+import endfieldindustrylib.Items.Powder;
+import endfieldindustrylib.Items.Industrial;
+import endfieldindustrylib.Items.Battery;
+import endfieldindustrylib.Items.Parts;
+import endfieldindustrylib.Items.Components;
+import endfieldindustrylib.Items.Bottle;
 
 public class Lib extends mindustry.mod.Mod {
     @Override
     public void loadContent() {
 
         // block
-        // 加载物流方块
-        AICTransport.load();
+        AICTransport.load();// 物流
         
         // item
-        // 加载矿物
-        Ore.load();
-        // 加载植物
-        Plants.load();
+        Ore.load();// 矿物
+        Plants.load();// 植物
+        Powder.load();// 粉末
+        Industrial.load();// 工业产物
+        Battery.load();// 电池
+        Parts.load();// 零件
+        Components.load();  // 装备原件
+        Bottle.load();// 瓶子
     }
 }
