08bf4a8123204319b95a4ed12071d1d4a791b2ef
Hollow-YK <normal.wzj@qq.com>
2026-02-22 19:10:47 +0800

    Add: AICBasicFacility:基础生产（四谷）

--- src/endfieldindustrylib/AICBasicFacility/SeedPickingUnit.java
+++ src/endfieldindustrylib/AICBasicFacility/SeedPickingUnit.java
@@ -9,7 +9,7 @@ public class SeedPickingUnit extends GenericAICBasicFacility {
         super(name);
 
         size = 5;
-        requirements(Category.production, ItemStack.with(Parts.amethystPart, 20));
+        requirements(Category.production, ItemStack.with(EFItems.amethystPart, 20));
 
         rotate = true;
         inputFacingMask = 1 << 2; // 背面输入
@@ -20,43 +20,43 @@ public class SeedPickingUnit extends GenericAICBasicFacility {
 
         recipes = new Recipe[]{
             new Recipe(
-                new ItemStack[]{ new ItemStack(Plants.buckflower, 1) },
-                new ItemStack[]{ new ItemStack(Plants.buckflowerSeed, 2) },
+                new ItemStack[]{ new ItemStack(EFItems.buckflower, 1) },
+                new ItemStack[]{ new ItemStack(EFItems.buckflowerSeed, 2) },
                 120f
             ),
             new Recipe(
-                new ItemStack[]{ new ItemStack(Plants.citrome, 1) },
-                new ItemStack[]{ new ItemStack(Plants.citromeSeed, 2) },
+                new ItemStack[]{ new ItemStack(EFItems.citrome, 1) },
+                new ItemStack[]{ new ItemStack(EFItems.citromeSeed, 2) },
                 120f
             ),
             new Recipe(
-                new ItemStack[]{ new ItemStack(Plants.aketine, 1) },
-                new ItemStack[]{ new ItemStack(Plants.aketineSeed, 2) },
+                new ItemStack[]{ new ItemStack(EFItems.aketine, 1) },
+                new ItemStack[]{ new ItemStack(EFItems.aketineSeed, 2) },
                 120f
             ),
             new Recipe(
-                new ItemStack[]{ new ItemStack(Plants.sandleaf, 1) },
-                new ItemStack[]{ new ItemStack(Plants.sandleafSeed, 2) },
+                new ItemStack[]{ new ItemStack(EFItems.sandleaf, 1) },
+                new ItemStack[]{ new ItemStack(EFItems.sandleafSeed, 2) },
                 120f
             ),
             new Recipe(
-                new ItemStack[]{ new ItemStack(Plants.tartpepper, 1) },
-                new ItemStack[]{ new ItemStack(Plants.tartpepperSeed, 2) },
+                new ItemStack[]{ new ItemStack(EFItems.tartpepper, 1) },
+                new ItemStack[]{ new ItemStack(EFItems.tartpepperSeed, 2) },
                 120f
             ),
             new Recipe(
-                new ItemStack[]{ new ItemStack(Plants.reedRye, 1) },
-                new ItemStack[]{ new ItemStack(Plants.reedRyeSeed, 2) },
+                new ItemStack[]{ new ItemStack(EFItems.reedRye, 1) },
+                new ItemStack[]{ new ItemStack(EFItems.reedRyeSeed, 2) },
                 120f
             ),
             new Recipe(
-                new ItemStack[]{ new ItemStack(Plants.redjadeGinseng, 1) },
-                new ItemStack[]{ new ItemStack(Plants.redjadeGinsengSeed, 2) },
+                new ItemStack[]{ new ItemStack(EFItems.redjadeGinseng, 1) },
+                new ItemStack[]{ new ItemStack(EFItems.redjadeGinsengSeed, 2) },
                 120f
             ),
             new Recipe(
-                new ItemStack[]{ new ItemStack(Plants.amberRice, 1) },
-                new ItemStack[]{ new ItemStack(Plants.amberRiceSeed, 2) },
+                new ItemStack[]{ new ItemStack(EFItems.amberRice, 1) },
+                new ItemStack[]{ new ItemStack(EFItems.amberRiceSeed, 2) },
                 120f
             )
         };
--- src/endfieldindustrylib/Lib.java
+++ src/endfieldindustrylib/Lib.java
@@ -2,28 +2,14 @@ package endfieldindustrylib;
 
 import endfieldindustrylib.AICBasicFacility.AICBasicFacility;
 import endfieldindustrylib.AICTransport.*;
-import endfieldindustrylib.Items.Ore;
-import endfieldindustrylib.Items.Plants;
-import endfieldindustrylib.Items.Powder;
-import endfieldindustrylib.Items.Industrial;
-import endfieldindustrylib.Items.Battery;
-import endfieldindustrylib.Items.Parts;
-import endfieldindustrylib.Items.Components;
-import endfieldindustrylib.Items.Bottle;
+import endfieldindustrylib.Items.EFItems;
 
 public class Lib extends mindustry.mod.Mod {
     @Override
     public void loadContent() {
         
         // item
-        Ore.load();// 矿物
-        Plants.load();// 植物
-        Powder.load();// 粉末
-        Industrial.load();// 工业产物
-        Battery.load();// 电池
-        Parts.load();// 零件
-        Components.load();  // 装备原件
-        Bottle.load();// 瓶子
+        EFItems.load();
 
         // block
         AICTransport.load();// 物流
